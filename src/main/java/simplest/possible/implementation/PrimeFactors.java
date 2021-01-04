2ae79b6689a7ee0558853ae7b18d69e391c4c8e7
Reghill J4 <rj4@lenovo.com>
2021-01-05 00:13:15 +0530

    Minimum implementation to make the test pass

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,10 @@
+package simplest.possible.implementation;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeFactors {
+    public <T> List<T> of(int number) {
+        return Collections.emptyList();
+    }
+}
